c1ebc91d873e8ca37af35e924eb7ab4617cbcec6
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-11 17:16:56 -0400

    assigning a variable a number and printing it

--- hello2/hello2/Program.fs
+++ hello2/hello2/Program.fs
@@ -1,8 +1,18 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿ // Learn more about F# at http://fsharp.org
 
 open System
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+
+    let x = 5
+
+    printfn "%d" x
+
+
     0 // return an integer exit code
+
+
+
+    
