12a7110bbf3bcd811c5c6d0063786eb504779417
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-11 19:09:27 -0400

    Calling a function

--- hello2/hello2/Program.fs
+++ hello2/hello2/Program.fs
@@ -11,7 +11,7 @@ let main argv =
 
     printfn "%d" x
 
-
+    let inp = System.Console.ReadLine()
     0 // return an integer exit code
 
 
---
+++ problemset1/problemset1/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+
+let succ n = n+1
+    
+
+[<EntryPoint>]
+let main argv =
+
+    let y = succ 4
+    printfn "%d" y
+
+    let inp = System.Console.ReadLine()
+    0 // return an integer exit code
+
+
+
