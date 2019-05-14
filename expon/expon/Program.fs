aa0aa29a1fdc76213baa3e36bb06b46d039f8e9c
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-14 18:37:01 -0400

    Looks like a indentation error but can not find it expon example

---
+++ expon/expon/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+
+let times n y = n*y
+
+let mk_expon times one =
+    let rec expon x n =
+        if n = 0 then one 
+        elif n%2 = expon (times x x) (n/2)
+        else times x (expon x (n-1)) 
+     expon 
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
