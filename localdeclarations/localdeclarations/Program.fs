cf126948cc7ab9de15d1af67cfbdeb7b06ac7078
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-15 19:37:03 -0400

    Creating times function in interactive

--- localdeclarations/localdeclarations/Program.fs
+++ localdeclarations/localdeclarations/Program.fs
@@ -5,4 +5,5 @@ open System
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let inp = System.Console.ReadLine()
     0 // return an integer exit code
--- localdeclarations/localdeclarations/Script1.fsx
+++ localdeclarations/localdeclarations/Script1.fsx
@@ -1,8 +1,13 @@
-﻿let y = 17 in 3*y;;
-val it : int = 51
+﻿
 
 // playing around in interactive 
 let x = 2 + 4
 let y = x;;
 val x : int = 6
 val y : int = 6
+
+
+let times x y = x*y
+let z = (times 2 4) + 4;;
+val times : x:int -> y:int -> int
+val z : int = 12
