d4999b1999b7b6f06191114c1ff2497580417eda
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-15 14:34:29 -0400

    attempting with match statemnet to understand match more

--- expon/expon/Program.fs
+++ expon/expon/Program.fs
@@ -11,6 +11,9 @@ let one = 1
 
 let mk_expon times one =
     let rec expon n x =
+        match n with 
+        |n=0 -> one 
+
         if n = 0 then one 
         elif n%2 = 0 then expon (times x x) (n/2)
         else times x (expon x (n-1))
