6ffb0b8e4a92338f1f67d6e443779edc6c6f7409
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-14 20:28:09 -0400

    Was able to fix indentation errors

--- expon/expon/Program.fs
+++ expon/expon/Program.fs
@@ -5,12 +5,17 @@ open System
 
 let times n y = n*y
 
+let one = 1
+
+
+
 let mk_expon times one =
-    let rec expon x n =
+    let rec expon n x =
         if n = 0 then one 
-        elif n%2 = expon (times x x) (n/2)
-        else times x (expon x (n-1)) 
-     expon 
+        elif n%2 = 0 then expon (times x x) (n/2)
+        else times x (expon x (n-1))
+    expon 
+
 
 [<EntryPoint>]
 let main argv =
