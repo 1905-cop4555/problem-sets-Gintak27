2082a33645babd765c438964ef2dc23882e66375
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-15 17:14:13 -0400

    getting weird errors not understanding the indentation

--- expon/expon/Program.fs
+++ expon/expon/Program.fs
@@ -10,14 +10,13 @@ let one = 1
 
 
 let mk_expon times one =
-    let rec expon n x =
-        match n with 
-        |n=0 -> one 
-
-        if n = 0 then one 
-        elif n%2 = 0 then expon (times x x) (n/2)
-        else times x (expon x (n-1))
-    expon 
+  let rec expon n x =
+    match n with 
+    |0 -> one 
+    |n when n%2 = 0 -> (times x x) (n/2)
+    |n -> (times x (expon x (n-1))
+  expon
+     
 
 
 [<EntryPoint>]
