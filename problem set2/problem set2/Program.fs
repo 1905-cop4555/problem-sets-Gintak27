360c1a9c7b449a82fb50df820bba1f913cdd4019
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-27 14:48:06 -0400

    Attempt at problem 1 writing a finction to convert uncurry to curry

---
+++ problem set2/problem set2/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let curry a = (fun x -> fun y -> a(x,y))
+
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
