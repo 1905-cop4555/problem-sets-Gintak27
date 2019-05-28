723a1e76b8c26658448031c832562b12cc9481ec
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-28 15:41:36 -0400

    attempting to use coverting function

--- problem set2/problem set2/Program.fs
+++ problem set2/problem set2/Program.fs
@@ -9,4 +9,7 @@ let curry a = (fun x -> fun y -> a(x,y))
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+
+    let curry f x y = f (x,y)
+    
     0 // return an integer exit code
