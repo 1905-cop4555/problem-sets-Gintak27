ee93c317aaac2b56a8a9145927483b28d64fcc1e
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-29 01:01:02 -0400

    split function

--- problem set2/problem set2/quicksorting.fsx
+++ problem set2/problem set2/quicksorting.fsx
@@ -1,10 +1,15 @@
 ﻿
 
 //create sort function rec
-
+let rec split pivot = function
+    | []    -> ([],[])
+    | x::xs -> let (left,right) = split pivot xs
+               if x < pivot then (x::left, right)
+                            else (left, x::right);;
 let rec qsort = function 
    |[] -> []
    |[x] -> [x]
-   |x::xs -> left 
+   |x::xs -> let (left,right) = split x xs
+             left @ x :: right;;
 
 //create  
