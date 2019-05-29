c288f632197272b66971bd96a1b0a3890cd0e0ba
Kai'Jana Henry <khenr040@fiu.edu>
2019-05-29 03:09:50 -0400

    complexity on reverse function

--- module2/module2/Script1.fsx
+++ module2/module2/Script1.fsx
@@ -4,3 +4,7 @@ let rec split = function
     | [x] -> ([x],[])
     | x::y::zs -> let (M,N) = split  zs
                   (x::M, y::N);;
+
+let rec rev = function
+   |[] -> []
+   |x::xs -> rev xs @ [x]
