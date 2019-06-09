2f0979d8c9aa36e61d5f9e537eb621248823c274
Kai'Jana Henry <khenr040@fiu.edu>
2019-06-09 17:33:45 -0400

    seeing if the tail recursive function works

--- module3.2/module3.2/ques5.fsx
+++ module3.2/module3.2/ques5.fsx
@@ -1,3 +1,9 @@
 ﻿let rec interleave = function
   | ([],[]) -> []
   | (x::xs, y::ys) -> x::y::(interleave (xs,ys));; 
+
+
+ let rec interleaveAux acc = function
+ | ([],[]) -> acc
+ | (x::xs,y::ys) -> let acc = x::y::acc
+                    interleaveAux acc (xs,ys)
