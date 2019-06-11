c5e0b22b0d914b0ce507c2af55448f8f5a4011a9
Kai'Jana Henry <khenr040@fiu.edu>
2019-06-10 21:57:19 -0400

    imperative factorial example

--- module3.2/module3.2/refexample.fsx
+++ module3.2/module3.2/refexample.fsx
@@ -4,3 +4,17 @@ let s = ref 18
 s := 8
 
 !s;;
+
+s := !s + 1;;
+
+!s;;
+
+let factorial n =
+    let ans = ref 1
+    let cnt = ref 1
+    while !cnt <= n do
+      ans := !ans * !cnt;
+      cnt := !cnt + 1
+    !ans
+
+factorial 5;;
