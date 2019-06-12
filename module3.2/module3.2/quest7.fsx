698dcae23d2e4a57e5f3855f219e587e9a65ac9b
Kai'Jana Henry <khenr040@fiu.edu>
2019-06-12 10:40:07 -0400

    creating functions on the stream

--- module3.2/module3.2/quest7.fsx
+++ module3.2/module3.2/quest7.fsx
@@ -8,3 +8,16 @@
   else filter p (xsf())
 
  let rec nextCons n = Cons (n, fun () -> filter p (nextCons (n+1)))
+
+ let rec findFirst n =
+  if (n % 2 = 0) && (n % 3 = 0) && (n % 21 = 0) && (n % 10 = 0) then n
+  else findFirst (n+1)
+ let sequence = nextCons (findFirst 1)
+
+ let rec take n (Cons (x, xsf)) =
+  if n = 0 then []
+  else x :: take (n-1) (xsf())
+
+ let rec skip n (Cons (x,xsf)) =
+  if n = 0 then Cons (x,xsf)
+  else skip (n-1) (xsf())
