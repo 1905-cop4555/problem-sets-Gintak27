8bdf6f42a967eeb797941100739889e96e6e254a
Kai'Jana Henry <khenr040@fiu.edu>
2019-06-12 04:24:11 -0400

    greating a sequence of floats

---
+++ module3.2/module3.2/quest6.fsx
@@ -0,0 +1,2 @@
+﻿ let infiniteSeq = Seq.initInfinite (fun index -> let n = float (index + 1)
+                                                  1.0 / ((2.0**n) * (if ((index + 1) % 2 = 0) then -1.0 else 1.0)));;
