31a9d0ba40a885171f8ac3dfdcbe4b86226b38ac
Kai'Jana Henry <khenr040@fiu.edu>
2019-06-12 04:25:48 -0400

    creating infinite stream

---
+++ module3.2/module3.2/quest7.fsx
@@ -0,0 +1,10 @@
+﻿type 'a Stream = Cons of 'a * (unit -> 'a Stream)
+
+ let p n =
+  (n % 2 = 0) && (n % 3 = 0) && (n % 21 = 0) && (n % 10 = 0)
+
+ let rec filter p (Cons (x, xsf)) =
+  if (p x) then Cons (x, fun () -> filter p (xsf()))
+  else filter p (xsf())
+
+ let rec nextCons n = Cons (n, fun () -> filter p (nextCons (n+1)))
